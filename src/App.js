5929c2b01fba652fd4b15f2cff2151e12b77484c
ltasker443 <107644753+ltasker443@users.noreply.github.com>
2022-09-02 16:38:36 -0400

    added github and netlify link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-///import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
       <h1>hello</h1>
       </header>
+      <footer className="App-footer">
+              <a href="https://github.com/ltasker443/react-dictionary" rel="noreferrer" target="_blank"> Open Source </a> by Laura Tasker & hosted on <a href="https://admirable-bunny-423ff4.netlify.app/" rel="noreferrer" target="_blank">Netlify</a>
+      </footer>
     </div>
   );
 }
