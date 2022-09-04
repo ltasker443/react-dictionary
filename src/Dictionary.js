47a39014a21e1207973e596f096a9b8bd0a2cb7f
ltasker443 <107644753+ltasker443@users.noreply.github.com>
2022-09-03 22:18:25 -0400

    added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,18 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function search(event) {
     event.preventDefault();
-    alert (`Summoning results for ${keyword}`);
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiURL).then(handleResponse);
     }
 
     function handleChange(event) {
